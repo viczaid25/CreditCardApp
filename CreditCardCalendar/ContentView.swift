f5eab7b22611b169c29da37ef29abd08367e41bf
Zaid Garcia <viczaidr@gmail.com>
2025-03-26 10:37:01 -0600

    Initial Commit

---
+++ CreditCardCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreditCardCalendar
+//
+//  Created by Zaid Garcia Casas on 26/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreditCardCalendar/CreditCardCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardCalendarApp.swift
+//  CreditCardCalendar
+//
+//  Created by Zaid Garcia Casas on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
